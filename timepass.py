9d268dbb496e5da91ea3a4f12b3c10303cf921b2
Genius Machado <geniusmachado@users.noreply.github.com>
2025-12-29 19:07:02 -0800

    list comprehension revision

---
+++ timepass.py
@@ -0,0 +1,11 @@
+print("today is just another day where I wathced python tutorials from youtube by Tech by Tim and then majorly I did linkedin Job applications")
+
+jokers = []
+def jobserarch():
+
+    platforms = [jokers.append(_) for _ in ['linkedin', 'workday', 'recruiters']]
+    print(platforms)
+    print(type(platforms))
+jobserarch()
+print(jokers)
+
