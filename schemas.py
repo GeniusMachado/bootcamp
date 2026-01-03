0928006315d0131b739e278c967b49c065d7ae9a
Genius Machado <geniusmachado@users.noreply.github.com>
2026-01-03 12:28:13 -0800

    Day3 of bootcamp: where we go through persisting data with SQLModel,session,create_engine,select classess from the sqlmodel module which we use to make a CRUD app

--- schemas.py
+++ schemas.py
@@ -1,7 +1,10 @@
-from pydantic import BaseModel
+from typing import Optional
+from sqlmodel import SQLModel, Field
 
-class Candidate(BaseModel):
+
+class Candidate(SQLModel, table=True):
+    id: Optional[int]= Field(default=None, primary_key=True)
     name: str
     years_experience: int
-    tech_stack: list[str]
+    tech_stack: str
     visa_sponsorship_needed: bool
